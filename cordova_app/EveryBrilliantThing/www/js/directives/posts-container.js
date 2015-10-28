02242faac1404505831b8eb96f590c1ec7229cd6
weilidai <weilidai2001@hotmail.com>
2015-10-28 21:36:57 +0000

    Deployed search bug fix to Cordova app

--- cordova_app/EveryBrilliantThing/www/js/directives/posts-container.js
+++ cordova_app/EveryBrilliantThing/www/js/directives/posts-container.js
@@ -1,15 +1,25 @@
-angular.module('PostsContainerDirective', [])
+angular.module('PostsContainerDirective', ['SearchService'])
 
 .directive('postsContainer', function($http) {
   return {
     templateUrl: 'partials/posts-container.html',
     restrict: 'A',
     scope: {},
-    controller: function($scope, $rootScope) {
+    controller: function($scope, $rootScope, Search) {
       $scope.posts = [];
       $scope.selectedPosts = [];
       $scope.searchFilter = '';
       $scope.searchTag = '';
+      $scope.searchVisible = false;
+
+      $rootScope.$watch(
+        function() {
+          return Search.isVisible();
+        },
+        function(newVal) {
+          $scope.searchVisible = newVal;
+        }
+      );
 
       $http
         .get('http://everybrilliantthing.tk/rest/posts')
