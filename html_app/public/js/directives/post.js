9d568333741d9bef305d35cdb4ae0e4f41efc2aa
weilidai <weilidai2001@hotmail.com>
2015-10-30 23:02:09 +0000

    Completed: Post-editor: after submission should scroll to the newly created post indicated by /?goto=17

--- html_app/public/js/directives/post.js
+++ html_app/public/js/directives/post.js
@@ -1,28 +1,40 @@
 angular.module('PostDirective', [])
 
-.directive('post', function() {
-  return {
-    templateUrl: 'partials/post.html',
-    restrict: 'A',
-    scope: {
-      data: '=',
-      selected: '='
-    },
-    controller: function($scope, $rootScope) {
-      function removeItemFromArray(arr, item) {
-        var index = arr.indexOf(item);
-        if (index > -1) {
-          arr.splice(index, 1);
-        }
-      }
+    .directive('post', ['$routeParams', function($routeParams) {
+      return {
+        templateUrl: 'partials/post.html',
+        restrict: 'A',
+        scope: {
+          data: '=',
+          selected: '='
+        },
+        link: function($scope, elem){
+          elem.ready(function(){
+            if($scope.data._id == $routeParams.goto){
+              console.log("Found top", $(elem).offset().top);
 
-      $scope.updateSelected = function() {
-        if ($scope.ticked) {
-          $scope.selected.push($scope.data._id);
-        } else {
-          removeItemFromArray($scope.selected, $scope.data._id);
+              $('html, body').animate({
+                scrollTop: $(elem).offset().top - 200
+              }, 500)
+
+            }
+          });
+        },
+        controller: function($scope, $rootScope) {
+          function removeItemFromArray(arr, item) {
+            var index = arr.indexOf(item);
+            if (index > -1) {
+              arr.splice(index, 1);
+            }
+          }
+
+          $scope.updateSelected = function() {
+            if ($scope.ticked) {
+              $scope.selected.push($scope.data._id);
+            } else {
+              removeItemFromArray($scope.selected, $scope.data._id);
+            }
+          };
         }
       };
-    }
-  };
-});
+    }]);
