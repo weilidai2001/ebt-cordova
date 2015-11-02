a37482d890fa8d37a681b7b02bba3fe59e39c9ee
cejast <Cem.Staveley@bbc.co.uk>
2015-11-02 17:20:58 +0000

    Added scroll to top button

--- cordova_app/EveryBrilliantThing/www/js/directives/posts-container.js
+++ cordova_app/EveryBrilliantThing/www/js/directives/posts-container.js
@@ -1,46 +1,52 @@
 angular.module('PostsContainerDirective', ['SearchService'])
+  .directive('postsContainer', ['$http', '$routeParams', function($http, $routeParams) {
+    return {
+      templateUrl: 'partials/posts-container.html',
+      restrict: 'A',
+      scope: {},
+      controller: function($scope, $rootScope, Search) {
+        $scope.posts = [];
+        $scope.selectedPosts = [];
+        $scope.searchFilter = '';
+        $scope.searchTag = '';
+        $scope.searchVisible = false;
+        $scope.tagsVisible = true;
 
-.directive('postsContainer', ['$http', '$routeParams', function($http, $routeParams) {
-  return {
-    templateUrl: 'partials/posts-container.html',
-    restrict: 'A',
-    scope: {},
-    controller: function($scope, $rootScope, Search) {
-      $scope.posts = [];
-      $scope.selectedPosts = [];
-      $scope.searchFilter = '';
-      $scope.searchTag = '';
-      $scope.searchVisible = false;
-
-      $rootScope.$watch(
-        function() {
-          return Search.isVisible();
-        },
-        function(newVal) {
-          $scope.searchVisible = newVal;
+        function filterByUrlQueryParams() {
+          if ($routeParams.ids) {
+            var visibleIds = $routeParams.ids.split("-");
+            angular.forEach($scope.posts, function(post) {
+              post.visible = visibleIds.indexOf(post._id.toString()) > -1;
+            });
+          } else {
+            angular.forEach($scope.posts, function(post) {
+              post.visible = true;
+            });
+          }
         }
-      );
 
-      console.log("RouteParams", $routeParams);
+        $rootScope.$watch(
+          function() {
+            return Search.isVisible();
+          },
+          function(newVal) {
+            $scope.searchVisible = newVal;
+          }
+        );
 
-      // TODO Find out how to listen to $viewContentLoaded event and use jQuery to scroll to /?goto=18
-      $scope.$on('$viewContentLoaded', function(){
-        //Here your view content is fully loaded !!
-        console.log("Content loaded");
-      });
+        $http
+          .get('http://everybrilliantthing.tk/rest/posts')
+          .success(function(response) {
+            $scope.posts = response;
+            filterByUrlQueryParams();
+            $scope.tagsVisible = !$routeParams.ids;
+          });
 
-
-      $http
-        .get('http://everybrilliantthing.tk/rest/posts')
-        .success(function(response) {
-          $scope.posts = response;
-        });
-
-      $http
-        .get('http://everybrilliantthing.tk/rest/tags')
-        .success(function(response) {
-          $scope.tags = response;
-        });
-    }
-  };
-}]);
+        $http
+          .get('http://everybrilliantthing.tk/rest/tags')
+          .success(function(response) {
+            $scope.tags = response;
+          });
+      }
+    };
+  }]);
--- html_app/public/js/directives/posts-container.js
+++ html_app/public/js/directives/posts-container.js
@@ -1,10 +1,10 @@
 angular.module('PostsContainerDirective', ['SearchService'])
-  .directive('postsContainer', ['$http', '$routeParams', function ($http, $routeParams) {
+  .directive('postsContainer', ['$http', '$routeParams', function($http, $routeParams) {
     return {
       templateUrl: 'partials/posts-container.html',
       restrict: 'A',
       scope: {},
-      controller: function ($scope, $rootScope, Search) {
+      controller: function($scope, $rootScope, Search) {
         $scope.posts = [];
         $scope.selectedPosts = [];
         $scope.searchFilter = '';
@@ -15,28 +15,28 @@ angular.module('PostsContainerDirective', ['SearchService'])
         function filterByUrlQueryParams() {
           if ($routeParams.ids) {
             var visibleIds = $routeParams.ids.split("-");
-            angular.forEach($scope.posts, function (post) {
+            angular.forEach($scope.posts, function(post) {
               post.visible = visibleIds.indexOf(post._id.toString()) > -1;
             });
           } else {
-            angular.forEach($scope.posts, function (post) {
+            angular.forEach($scope.posts, function(post) {
               post.visible = true;
             });
           }
         }
 
         $rootScope.$watch(
-          function () {
+          function() {
             return Search.isVisible();
           },
-          function (newVal) {
+          function(newVal) {
             $scope.searchVisible = newVal;
           }
         );
 
         $http
           .get('http://everybrilliantthing.tk/rest/posts')
-          .success(function (response) {
+          .success(function(response) {
             $scope.posts = response;
             filterByUrlQueryParams();
             $scope.tagsVisible = !$routeParams.ids;
@@ -44,7 +44,7 @@ angular.module('PostsContainerDirective', ['SearchService'])
 
         $http
           .get('http://everybrilliantthing.tk/rest/tags')
-          .success(function (response) {
+          .success(function(response) {
             $scope.tags = response;
           });
       }
