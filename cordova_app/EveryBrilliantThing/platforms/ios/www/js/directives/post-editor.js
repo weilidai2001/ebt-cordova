a0e6d3e81e352e5a0257f8d638fcf9811ebb13d4
weilidai <weilidai2001@hotmail.com>
2015-11-03 21:39:26 +0000

    Updated checkboxes to be round

--- cordova_app/EveryBrilliantThing/platforms/ios/www/js/directives/post-editor.js
+++ cordova_app/EveryBrilliantThing/platforms/ios/www/js/directives/post-editor.js
@@ -1,73 +1,5 @@
 angular.module('PostEditorDirective', [])
 
-<<<<<<< HEAD
-    .directive('postEditor', ['$http', '$window', '$location', function($http, $window, $location) {
-        return {
-            templateUrl: 'partials/post-editor.html',
-            restrict: 'A',
-            scope: {
-
-            },
-            controller: function($scope) {
-                $scope.style = 1;
-                $scope.color = 1;
-                $scope.postBody = '';
-                $scope.colorSelectionVisible = false;
-                $scope.styleSelectionVisible = false;
-                $http
-                    .get(paths.tagsEndpoint)
-                    .success(function (response) {
-                        $scope.tags = response;
-                    });
-
-                $scope.setColor = function(color) {
-                    $scope.color = color;
-                    $scope.colorSelectionVisible = false;
-                };
-
-                $scope.setStyle = function(style) {
-                    $scope.style = style;
-                    $scope.styleSelectionVisible = false;
-                };
-
-                $scope.setTag = function(event) {
-                    var selectedElement = event.srcElement;
-                    $scope.selectedTag = $(selectedElement).text();
-                    $scope.tagSelectionVisible = false;
-                };
-
-                $scope.hidePostEditor = function() {
-                    $scope.postEditorVisible = false;
-                };
-
-                $scope.submitPost = function(postBody) {
-                    if (postBody.length === 0) {
-                        return $window.alert('Post cannot be blank');
-                    }
-                    var post = {
-                        style: $scope.style,
-                        color: $scope.color,
-                        tag: $scope.selectedTag == "Add a tag" ? "" : $scope.selectedTag,
-                        body: postBody
-                    };
-                    resetPostEditor();
-                    $http.post(paths.postsEndpoint, post).success(function(data, status){
-                        console.log('data:', data, 'status:', status);
-                        $location.path("/").search({goto: data.newId});
-                    });
-                };
-
-                function resetPostEditor() {
-                    $scope.style = 1;
-                    $scope.color = 1;
-                    $scope.postBody = '';
-                    $scope.colorSelectionVisible = false;
-                    $scope.styleSelectionVisible = false;
-                }
-            }
-        };
-    }]);
-=======
 .directive('postEditor', ['$http', '$window', '$location', function($http, $window, $location) {
   return {
     templateUrl: 'partials/post-editor.html',
@@ -135,4 +67,3 @@ angular.module('PostEditorDirective', [])
     }
   };
 }]);
->>>>>>> e072aae70a7e1d230bc18d8e0faf1eb4dc95caec
