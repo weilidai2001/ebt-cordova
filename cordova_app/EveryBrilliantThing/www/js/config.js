d3e697c15bc2706d5adb83536939203fab5ae182
weilidai <weilidai2001@hotmail.com>
2015-12-18 20:04:55 +0000

    Moved Phonegap app to a separate standalone project

--- cordova_app/EveryBrilliantThing/www/js/config.js
+++
@@ -1,6 +0,0 @@
-var paths = {
-  dailySpotlightEndpoint: "http://everybrilliantthing.tk/rest/dailySpotlight",
-  postsEndpoint: "http://everybrilliantthing.tk/rest/posts",
-  postItImagesFolder: "http://everybrilliantthing.tk/img/post-it/",
-  tagsEndpoint: "http://everybrilliantthing.tk/rest/tags"
-};
--- cordova_app/EveryBrilliantThing/www/js/directives/about.js
+++
@@ -1,7 +0,0 @@
-angular.module('AboutDirective', [])
-  .directive('about', function() {
-    return {
-      templateUrl: 'partials/about-us.html',
-      restrict: 'A'
-    };
-  });
